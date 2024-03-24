b19ca1acce4bf8197ca712537e7e7358f32ec884
Mahad Hassan <mahadh578@gmail.com>
2024-03-24 18:48:53 -0500

    Worked on assignment 5.3 and forgot to commit throughout assiignment

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,10 +1,51 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+let myCollection = [];
+function addToCollection(collection, title, artist, yearPublished) {
+let Album = {
+    title: title ,
+    artist: artist,
+    yearPublished: yearPublished
+}
+collection.push (Album);
+return Album; 
+}
+
+let album0 = addToCollection(myCollection, "Eternal Atake", "Lil Uzi Vert", 2020);
+console.log(album0);
+let album1 = addToCollection(myCollection, "Whole Lotta Red", "Playboi Carti", 2020);
+console.log(album1);
+let album2 = addToCollection(myCollection, "Get Rich or Die Tryin'", "50 Cent", 2003);
+console.log(album2);
+let album3 = addToCollection(myCollection, "The Life of Pi'erre 4", "Pierre Bourne", 2019);
+console.log(album3);
+let album4 = addToCollection(myCollection, "Wunna", "Gunna", 2020);
+console.log(album4);
+let album5 = addToCollection(myCollection, "My Turn", "Lil Baby", 2020);
+console.log(album5);
 
 
+console.log(myCollection);
 
+function showCollection(collection) {
+  for (let i = 0; i < collection.length; i++) {
+    let album = collection[i];
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}.`);
+  }
+}
+function findByArtist(collection, artist) {
+  let matchingAlbums = [];
 
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].artist === artist) {
+      matchingAlbums.push(collection[i]);
+    }
+  }
+  return matchingAlbums;
+}
+console.log(findByArtist(myCollection, "Lil Uzi Vert"));
 
+console.log(findByArtist(myCollection, "Kendrick Lamar"));
 
 
 
